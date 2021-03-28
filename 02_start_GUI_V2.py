ee5080df4802c3980ce54af1b1de41dfd9bee9d7
willoughbys70590 <60673309+willoughbys70590@users.noreply.github.com>
2021-03-29 09:53:57 +1300

    working buttons low medium and high

--- 02_start_GUI_V2.py
+++ 02_start_GUI_V2.py
@@ -39,19 +39,19 @@ class Start:
         self.lowstakes_button = Button(self.stakes_frame, text="low($5)",
                                        command=lambda: self.to_game(1),
                                        font=button_font, bg="#FF9933")
-        self.lowstakes_button.grid(row=0, colum=0, pady=10)
+        self.lowstakes_button.grid(row=0, column=0, pady=10)
 
         # Yellow medium stakes button...
-        self.lowstakes_button = Button(self.stakes_frame, text="Medium ($10)",
+        self.mediumstakes_button = Button(self.stakes_frame, text="Medium ($10)",
                                        command=lambda: self.to_game(2),
                                        font=button_font, bg="#FFFF33")
-        self.lowstakes_button.grid(row=0, colum=1, padx=5, pady=10)
+        self.mediumstakes_button.grid(row=0, column=1, padx=5, pady=10)
 
         # Green high stakes button
-        self.lowerstakes_button = Button(self.stakes_frame, text="High ($15)",
+        self.highstakes_button = Button(self.stakes_frame, text="High ($15)",
                                          command=lambda: self.to_game(3),
                                          font=button_font, bg="#99FF33")
-        self.lowstakes_button.grid(row=0, colum=2, pady=10)
+        self.highstakes_button.grid(row=0, column=2, pady=10)
 
         # Help Button
         self.help_button = Button(self.start_frame, text="How to play",
@@ -62,53 +62,18 @@ class Start:
         starting_balance = self.start_amount_entry.get()
         Game(self, stakes, starting_balance)
 
+        # Set error background colours (self assume that there are no
+        # Errors at the start...
+        error_back = "#ffafaf"
+        has_errors = "no"
+
+        # Change backgrounds to white  (for testing purposes)
 
 class Game:
     def __init__(self, partner, stake, starting_balance):
         print(stake)
         print(starting_balance)
 
-        # disable low stakes button
-        partner.lowstakes_button.config(state=DISABLED)
-
-        # initialise variables
-        self.balance = IntVar()
-
-        # GUI setup
-        self.game_box = Toplevel()
-        self.game_frame = Frame(self.game_box)
-        self.game_frame.grid()
-
-        # Heading Row
-        self.heading_label = Label(self.game_frame, text="Heading",
-                                    font="Arial 24 bold", padx=10,
-                                    pady=10)
-        self.heading_label.grid(row=0)
-
-        # Balance Label
-        self.balance_frame = Label(self.game_frame)
-        self.balance_frame.grid(row=1)
-
-        self.balance_label = Label(self.game_frame, text="Balance...")
-        self.balance_label.grid(row=2)
-
-        self.play_button = Button(self.game_frame, text="Gain",
-                                    padx=10, pady=10, command=self.reveal_boxes)
-        self.play_button.grid(row=3)
-
-    def reveal_boxes(self):
-        # retrieve the balance from the initial function...
-        current_balance = self.balance.get()
-
-        # Adjust the balance (subtract game cost and add pay out)
-        # For testing purposes, just add 2
-        current_balance += 2
-
-        # Set balance to adjusted balance
-        self.balance.set(current_balance)
-
-        # Edit label so user can see their balance
-        self.balance_label.configure(text="Balance: {}".format(current_balance))
 
 # main routine
 if __name__ == "__main__":
